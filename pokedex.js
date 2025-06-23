83257b19a1c34a07c5635cd442b702251d550371
Aluno.Etec <aula@etec.com>
2025-06-23 14:41:16 -0300

    finalizando a pokedex adicionando peso,altura e evolução

--- pokedex.js
+++ pokedex.js
@@ -1,70 +1,123 @@
+var NomePokemon = document.querySelector('.pokenome');
+var NumPokemon = document.querySelector('.pokenum');
+var ImgPokemon = document.querySelector('#pokemon');
 
-const NomePokemon = document.querySelector('.pokenome');
-const NumPokemon = document.querySelector('.pokenum');
-const ImgPokemon = document.querySelector('#pokemon');
- 
-const form = document.querySelector('#formulario');
-const input = document.querySelector('#pesquisarpokemon');
- 
-const buttonprev = document.querySelector('.btn-prev');
-const buttonnext = document.querySelector('.bnt-next');
- 
- 
- 
- 
- 
-let searchpokemon = 1;
- 
-const fetchPokemon = async (pokemon) => {
- 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-   
-   
-    if(APIResponse.status == 200){
+var form = document.querySelector('#formulario');
+var input = document.querySelector('#pesquisarpokemon');
+
+var buttonprev = document.querySelector('.btn-prev');
+var buttonnext = document.querySelector('.bnt-next');
+
+var habilidade0 = document.querySelector('.habilidade0');
+var habilidade1 = document.querySelector('.habilidade1');
+var tipopokemon = document.querySelector('#tipo');
+
+var pesopokemon = document.querySelector('#peso');
+var alturapokemon = document.querySelector('#altura');
+
+var nao = document.querySelector('#nao');
+
+
+var fetchPokemon = async (pokemon) => {
+    var APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (APIResponse.status == 200) {
         const data = await APIResponse.json();
         return data;
-    }
-    else{
+    } else {
         NomePokemon.innerHTML = 'Não encontrado!';
         NumPokemon.innerHTML = '';
+        return null;
     }
- 
-}
- 
-const renderPokemon = async (pokemon) => {
-    const data = await fetchPokemon(pokemon);
-    if (data){
-    NomePokemon.innerHTML = data.name;
-    NumPokemon.innerHTML = data.id;
-   
-    ImgPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    searchpokemon = data.id;
-}
+};
+
+
+const buscarEvolucao = async (pokemon) => {
+    const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
+    const speciesData = await speciesResponse.json();
+
+    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionResponse.json();
+
+    let evolucaoNome = null;
+    let cadeia = evolutionData.chain;
+
+    if (cadeia.species.name === pokemon.toLowerCase()) {
+        if (cadeia.evolves_to.length > 0) {
+            evolucaoNome = cadeia.evolves_to[0].species.name;
+        }
+    } else if (
+        cadeia.evolves_to.length > 0 &&
+        cadeia.evolves_to[0].species.name === pokemon.toLowerCase()
+    ) {
+        if (cadeia.evolves_to[0].evolves_to.length > 0) {
+            evolucaoNome = cadeia.evolves_to[0].evolves_to[0].species.name;
+        }
     }
-   
- 
+
+    if (!evolucaoNome) return null;
+
+    const evoResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${evolucaoNome}`);
+    const evoData = await evoResponse.json();
+
+    return {
+        nome: evolucaoNome,
+        id: evoData.id
+    };
+};
+
+
+var renderPokemon = async (pokemon) => {
+    var data = await fetchPokemon(pokemon);
+    if (data) {
+        NomePokemon.innerHTML = data.name;
+        NumPokemon.innerHTML = data.id;
+
+        ImgPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+        habilidade0.innerHTML = data.abilities[0].ability.name;
+        habilidade1.innerHTML = data.abilities.length > 1 ? data.abilities[1].ability.name : 'N/A';
+
+        tipopokemon.innerHTML = data.types[0].type.name;
+
+
+        const altura = data.height / 10;
+        const peso = data.weight / 10;
+        alturapokemon.innerHTML = `Altura: ${altura} m`;
+        pesopokemon.innerHTML = `Peso: ${peso} kg`;
+
+
+        const evolucao = await buscarEvolucao(data.name);
+
+        if (evolucao) {
+            document.querySelector('#evolucao').innerHTML = `Evolução: ${evolucao.nome}`;
+            document.querySelector('#img-evolucao').src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${evolucao.id}.gif`;
+        } else {
+            document.querySelector('#evolucao').innerHTML = 'Evolução: Ele não evolui ╯︿╰';
+            document.querySelector('#img-evolucao').src = '#';
+        }
+
+        searchpokemon = data.id;
+    }
+};
+
+var searchpokemon = 1;
+
 form.addEventListener('submit', (event) => {
- 
     event.preventDefault();
- 
-    renderPokemon (input.value);
- 
+    renderPokemon(input.value.toLowerCase());
 });
- 
+
 buttonprev.addEventListener('click', () => {
- 
-   searchpokemon -= 1;
-    renderPokemon(searchpokemon);
- 
+    if (searchpokemon > 1) {
+        searchpokemon -= 1;
+        renderPokemon(searchpokemon);
+    }
 });
- 
+
 buttonnext.addEventListener('click', () => {
- 
     searchpokemon += 1;
     renderPokemon(searchpokemon);
- 
 });
- 
+
 renderPokemon(searchpokemon);
- 
- 
